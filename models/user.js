e329f5bef6833d8df72dc6b74735f0406a4023c6
dikinuwa007 <dikinuwa@gmail.com>
2023-09-28 12:38:23 +0700

    test

--- models/user.js
+++ models/user.js
@@ -45,6 +45,7 @@ module.exports = (sequelize, DataTypes) => {
     email: {
     type:DataTypes.STRING,
     allowNull:false,
+    unique:true,
     validate:{
       notNull:{
         msg:'email required'  
